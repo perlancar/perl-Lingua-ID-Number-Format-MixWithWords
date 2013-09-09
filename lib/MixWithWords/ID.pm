6ea4f79850cba07ddaf4d31a8423dcb55cdb21e3
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-09-09 18:22:17 +0700

    Release v0.05 (Rebuild with MakeMaker)

--- lib/MixWithWords/ID.pm
+++ lib/MixWithWords/ID.pm
@@ -5,5 +5,6 @@ use Package::Rename qw(link_package);
 
 link_package "Lingua::ID::Number::Format::MixWithWords", "MixWithWords::ID";
 
+# VERSION
 1;
 # ABSTRACT: Alias for Lingua::ID::Number::Format::MixWithWords
